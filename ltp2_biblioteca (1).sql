09a595d2e49f6abddd535774f00a82b1e92522b8
João Gabriel <gsantana152@gmail.com>
2017-12-03 19:58:06 -0300

    Add files via upload

---
+++ ltp2_biblioteca (1).sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 03-Dez-2017 às 23:55
+-- Versão do servidor: 10.1.28-MariaDB
+-- PHP Version: 7.1.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ltp2_biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `biblioteca`
+--
+
+CREATE TABLE `biblioteca` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `biblioteca`
+--
+
+INSERT INTO `biblioteca` (`id`, `nome`) VALUES
+(4, 'Bib 2'),
+(6, 'Bib 4'),
+(7, 'Bib 5');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `biblioteca_livro`
+--
+
+CREATE TABLE `biblioteca_livro` (
+  `biblioteca_id` int(11) DEFAULT NULL,
+  `livro_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `livro`
+--
+
+CREATE TABLE `livro` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `editora` varchar(50) NOT NULL,
+  `edicao` varchar(50) NOT NULL,
+  `area` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `livro`
+--
+
+INSERT INTO `livro` (`id`, `nome`, `editora`, `edicao`, `area`) VALUES
+(4, 'nome', 'editora', 'edicao', 'area'),
+(5, 'nome2', 'editora2', 'edicao2', 'area2'),
+(8, 'nome5', 'editora5', 'edicao5', 'area5');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `nome` (`nome`);
+
+--
+-- Indexes for table `biblioteca_livro`
+--
+ALTER TABLE `biblioteca_livro`
+  ADD KEY `fk_biblioteca` (`biblioteca_id`),
+  ADD KEY `fk_livro` (`livro_id`);
+
+--
+-- Indexes for table `livro`
+--
+ALTER TABLE `livro`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `biblioteca`
+--
+ALTER TABLE `biblioteca`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `livro`
+--
+ALTER TABLE `livro`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Limitadores para a tabela `biblioteca_livro`
+--
+ALTER TABLE `biblioteca_livro`
+  ADD CONSTRAINT `fk_biblioteca` FOREIGN KEY (`biblioteca_id`) REFERENCES `biblioteca` (`id`),
+  ADD CONSTRAINT `fk_livro` FOREIGN KEY (`livro_id`) REFERENCES `livro` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
